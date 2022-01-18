7dc0f5550457e9ef59adda13fa21321b3c54b195
Syed Sharukh Hussainy <syedsharukhhussainy@gmail.com>
2022-01-18 13:13:13 +0530

    created TripList component and added custom hooks for fetching data from the API

---
+++ src/components/TripList.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import useFetch from "../hooks/useFetch";
+import "./TripList.css";
+
+export default function TripList() {
+  const [url, setUrl] = useState("http://localhost:3000/trips");
+
+  const { data: trips, isPending, error } = useFetch(url, { type: "GET" });
+
+  return (
+    <div className="trip-list">
+      <h2>Trip List</h2>
+      <ul>
+        {isPending && <div className="loading">Loading..</div>}
+        {error && <div className="error">{error}</div>}
+        {trips &&
+          trips.map((trip) => (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          ))}
+      </ul>
+      <div className="filters">
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trip
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,40 @@
+import { useState, useEffect, useRef } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, { signal: controller.signal });
+        if (!response.ok) throw new Error(response.statusText);
+        const json = await response.json();
+        setIsPending(false);
+        setData(json);
+        setError(null);
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log("The fetch was aborted");
+        } else {
+          setIsPending(false);
+          setError("Could not fetch the data");
+        }
+      }
+    };
+    fetchData();
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  return {
+    data,
+    isPending,
+    error,
+  };
+};
+
+export default useFetch;
